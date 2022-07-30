a26e832591a3d77461e3860b522a1b10f999515c
JD_Zhong <jd_zhong@qq.com>
2022-07-30 15:30:48 +0800

    2022-07-30-15-30

---
+++ daily/src/LargestComponentSize.java
@@ -0,0 +1,69 @@
+import java.util.Arrays;
+
+/**
+ * 952. 按公因数计算最大组件大小
+ * https://leetcode.cn/problems/largest-component-size-by-common-factor/
+ */
+public class LargestComponentSize {
+    public static void main(String[] args) {
+        int[] nums = {2,3,6,7,4,12,21,39};
+    }
+
+    // 暴力建图+BFS会超时
+    // 使用并查集
+    public int largestComponentSize(int[] nums) {
+        int m = Arrays.stream(nums).max().getAsInt();
+        UnionFind uf = new UnionFind(m + 1);
+        for (int num : nums) {
+            for (int i = 2; i * i <= num; i++) {
+                if (num % i == 0) {
+                    uf.union(num, i);
+                    uf.union(num, num / i);
+                }
+            }
+        }
+        int[] counts = new int[m + 1];
+        int ans = 0;
+        for (int num : nums) {
+            int root = uf.find(num);
+            counts[root]++;
+            ans = Math.max(ans, counts[root]);
+        }
+        return ans;
+    }
+}
+
+class UnionFind {
+    int[] parent;
+    int[] rank;
+
+    public UnionFind(int n) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        rank = new int[n];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+}
